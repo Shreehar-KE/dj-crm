2ec084729b72e4233d8daa8c31cbaacd265e4f92
Shreehar-KE <shreehar.elancheran@gmail.com>
2024-04-28 22:39:28 +0530

    add users app

--- apps/users/apps.py
+++ apps/users/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LogsConfig(AppConfig):
+class UsersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.logs'
+    name = 'users'
